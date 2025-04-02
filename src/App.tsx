dd88d6610b2621024f753e227daaea774e6a5d5a
prince15130 <prince.bakshi2000@gmail.com>
2025-04-03 01:19:02 +0530

    added sidebar functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import { MainContent } from "./components/main-content";
+import Sidebar from "./components/sidebar";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <div className="h-screen flex px-3 py-4 bg-[3F3F5F7]">
+        <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
         <MainContent />
       </div>
     </>
